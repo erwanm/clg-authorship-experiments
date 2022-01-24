096f89ff6596d90be2a238006565af91c1e35560
Erwan Moreau <erwan.more@gmail.com>
2022-01-24 23:40:33 +0000

    starting

---
+++ session-setup.sh
@@ -0,0 +1,8 @@
+if [ -z "$CLG_AUTHORSHIP_PACKAGES_PATH" ]; then
+    echo "$0: ERROR, environment variable \$CLG_AUTHORSHIP_PACKAGES_PATH must be set." 1>&2
+    exit 1
+fi
+
+source "$CLG_AUTHORSHIP_PACKAGES_PATH"/erw-bash-commons/lib/init-erw-pm.sh
+erw-pm addrepo "$CLG_AUTHORSHIP_PACKAGES_PATH"
+erw-pm activate clg-authorship-analytics
