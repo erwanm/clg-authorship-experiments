dbdcaa74c763c84b0e43e798dc0a05e54255d873
Erwan Moreau <moreaue@cs.tcd.ie>
2022-02-01 18:58:35 +0000

    slow progress

--- session-setup.sh
+++ session-setup.sh
@@ -3,6 +3,9 @@ if [ -z "$CLG_AUTHORSHIP_PACKAGES_PATH" ]; then
     exit 1
 fi
 
+export PERL5LIB=$PERL5LIB:$ADD_TO_PERL5LIB
+
 source "$CLG_AUTHORSHIP_PACKAGES_PATH"/erw-bash-commons/lib/init-erw-pm.sh
 erw-pm addrepo "$CLG_AUTHORSHIP_PACKAGES_PATH"
 erw-pm activate clg-authorship-analytics
+
